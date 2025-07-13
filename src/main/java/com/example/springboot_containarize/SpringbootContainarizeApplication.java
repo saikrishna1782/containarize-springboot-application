115f1cfff4fba7e933299509724431ad1ab7b1c6
saikrishna1782 <saikrishnak.0705@gmail.com>
2025-07-13 18:09:21 -0500

    containarize springboot application

---
+++ src/main/java/com/example/springboot_containarize/SpringbootContainarizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_containarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootContainarizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootContainarizeApplication.class, args);
+	}
+
+}
