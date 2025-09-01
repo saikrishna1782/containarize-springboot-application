d3e1691f14e54b7001fd1c98013c18b2dd63e34a
saikrishna1782 <saikrishnak.0705@gmail.com>
2025-09-01 17:02:50 -0500

    added Data Request

--- src/main/java/com/example/springboot_containarize/DataRequest.java
+++ src/main/java/com/example/springboot_containarize/DataRequest.java
@@ -1,4 +1,69 @@
 package com.example.springboot_containarize;
 
 public class DataRequest {
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+    private String message;
+
+    // Default constructor
+    public DataRequest() {}
+
+    // All-args constructor
+    public DataRequest(int id, String name, String email, int age, String message) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.message = message;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // For debugging / logging
+    @Override
+    public String toString() {
+        return "DataRequest{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
