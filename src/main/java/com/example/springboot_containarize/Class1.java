d2cd53443674e2f5e173f4463b696900060f6cf3
saikrishna1782 <saikrishnak.0705@gmail.com>
2026-05-30 18:27:29 -0500

    added a class file

---
+++ src/main/java/com/example/springboot_containarize/Class1.java
@@ -0,0 +1,15 @@
+package com.example.springboot_containarize;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Class1 {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public String status() {
+        return "jebfd efjbfr fk3nr i13nrek23";
+    }
+}
